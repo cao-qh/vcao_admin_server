ca95c53df8c0fe0d10dc51dd2ab94b21fb22c622
Cao Qihao <caoqihao88@gmail.com>
2025-01-19 14:43:57 +0800

    测试git 配置

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bottle import route, run
 @route('/')
 @route('/hello')
 def hello():
-    return "Hello World!"
+    return "Hello World!!!"
 
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True)
